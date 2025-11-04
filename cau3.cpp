cd9002578395fabd2e354852b3901173a4bb7fe0
ndh93 <ndhoan2005@gmail.com>
2025-11-04 22:07:07 +0700

    thêm chú thích

--- cau3.cpp
+++ cau3.cpp
@@ -9,15 +9,15 @@ string maHoaChuoi(const string &s) {
 
     // Duyệt từ ký tự thứ 2 (index = 1) đến hết chuỗi
     for (int i = 1; i <= s.length(); i++) {
-        // Nếu ký tự hiện tại giống ký tự trước đó → tăng biến đếm
+        // Nếu ký tự hiện tại giống ký tự trước đó thì tăng biến đếm
         if (i < s.length() && s[i] == s[i - 1]) {
             dem++;
         } 
-        // Nếu khác hoặc đã đến cuối chuỗi → ghi vào kết quả
+        // Nếu khác hoặc đã đến cuối chuỗi thì ghi vào kết quả
         else {
             result += s[i - 1];      // thêm ký tự
             result += to_string(dem); // thêm số lần lặp
-            dem = 1; // reset biến đếm cho ký tự mới
+            dem = 1; // đặt lại biến đếm cho ký tự mới
         }
     }
 
